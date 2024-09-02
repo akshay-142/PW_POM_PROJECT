210938226510abae03433a2c08879e1781b51cbf
qa-akshay-pb <100873144+akshay-142@users.noreply.github.com>
2024-09-02 23:01:56 +0530

    add schedule

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,7 @@ const { defineConfig, devices } = require("@playwright/test");
 module.exports = defineConfig({
   timeout: 50000,
   testDir: "./tests",
+  reporter: [["list"], ["allure-playwright"]],
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,7 +23,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  //reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
